fc13ecc478083cc89d2952852c0c6ca266f549a3
giuliofantuzzi <giulio.fantuzzi01@gmail.com>
2024-01-24 21:52:30 +0100

    added bash scripts for bcast chain and binary tree

---
+++ exercise1/bcast/bcast_binarytree/bcast_binarytree.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# move inside the directory where to perform benchmarking
+exe_filepath=../../osu-micro-benchmarks-7.3/c/mpi/collective/blocking/
+
+# Specify the output CSV file and set its header
+csv_file="../bcast_results/bcast_binarytree.csv"
+echo "Algorithm,Processes,AvgLatency(us),MinLatency,MaxLatency" >> $csv_file
+
+for np in {2..256..1}
+    do
+    # Construct the mpirun command with the current values of np and algorithm
+    command="mpirun -np $np --map-by core --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 5 ${exe_filepath}osu_bcast -x 100 -i 100 -m 1:1 -f"
+    # Execute the command, extract the numbers and append to the CSV file
+    echo "Currentlty benchmarking with n. of processes= $np"
+    latency_stats=$(eval $command | tail -n 1 | awk '{gsub(/ +/, ","); sub(/^[^,]+,/, ""); sub(/,[^,]+$/, ""); print}')
+    echo "binarytree,$np,$latency_stats" >> $csv_file
+done
+
+
