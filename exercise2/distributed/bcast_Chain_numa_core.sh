76fbd45b71a44922b4bedfcba029470ee98b9a27
Giulio Fantuzzi <gfantuzzi@login01.hpc.rd.areasciencepark.it>
2024-01-27 15:49:07 +0000

    data map by numa rank by core

---
+++ exercise2/distributed/bcast_Chain_numa_core.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Remember to alloc a node!
+# Compile the C program
+src=src/bcast_Chain.c
+exe=exe/bcast_Chain_numa_core
+out_csv=results/bcast_Chain_numa_core.csv
+
+mkdir exe
+touch $out_csv
+srun mpicc $src -o $exe
+
+# Create a CSV file and add a header
+echo "Algorithm,Processes,AvgTime" > $out_csv
+
+# Loop over different numbers of processes
+for processes in {2..128..1}; do
+    echo "Benchmarking Chain with np=$processes..."
+    # Run the MPI program with mpirun and save the average time to the CSV file
+    result=$(mpirun -np $processes --map-by numa --rank-by core $exe)
+    echo "Chain,$processes,$result" >> $out_csv
+done
+
+#Remove the executable directory
+rm -r exe
