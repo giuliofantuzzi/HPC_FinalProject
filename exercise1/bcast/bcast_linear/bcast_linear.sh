4005074c6bb5ec7f69675e959d2c6302e8c8c4ef
giuliofantuzzi <giulio.fantuzzi01@gmail.com>
2024-01-28 18:37:28 +0100

    fixedbash scripts for processes data

--- exercise1/bcast/bcast_linear/bcast_linear.sh
+++ exercise1/bcast/bcast_linear/bcast_linear.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-# move inside the directory where to perform benchmarking
-exe_filepath=../../osu-micro-benchmarks-7.3/c/mpi/collective/blocking/
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=128
+#SBATCH --time=02:00:00
+#SBATCH --partition=EPYC
+#SBATCH --job-name=HPC_exam
 
+# move inside the directory where to perform benchmarking
+src_path="../../osu-micro-benchmarks-7.3/c/mpi/collective/blocking/"
 # Specify the output CSV file and set its header
-csv_file="../bcast_results/bcast_linear.csv"
-echo "Algorithm,Processes,AvgLatency(us),MinLatency,MaxLatency" >> $csv_file
+out_csv="../bcast_results/bcast_linear.csv"
+echo "Algorithm,Processes,AvgLatency(us),MinLatency,MaxLatency" >> $out_csv
 
 for np in {2..256..1}
-    do
+do
     # Construct the mpirun command with the current values of np and algorithm
-    command="mpirun -np $np --map-by core --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 1 ${exe_filepath}osu_bcast -x 1000 -i 10000 -m 1:1 -f"
+    command="mpirun -np $np --map-by core --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 1 ${src_path}osu_bcast -x 1000 -i 10000 -m 1:1 -f"
     # Execute the command, extract the numbers and append to the CSV file
-    echo "Currentlty benchmarking with n. of processes= $np"
+    echo "Currentlty benchmarking Linear with n. of processes= $np"
     latency_stats=$(eval $command | tail -n 1 | awk '{gsub(/ +/, ","); sub(/^[^,]+,/, ""); sub(/,[^,]+$/, ""); print}')
-    echo "linear,$np,$latency_stats" >> $csv_file
+    echo "Linear,$np,$latency_stats" >> $out_csv
 done
 
 
