ac30bb99e7cbed4ad1e3ac99517f9e56bd070cb5
Giulio Fantuzzi <gfantuzzi@login01.hpc.rd.areasciencepark.it>
2024-01-30 19:23:41 +0000

    got data for bind by core

--- exercise1/bcast_THIN/bcast_binarytree/bcast_binarytree.sh
+++ exercise1/bcast_THIN/bcast_binarytree/bcast_binarytree.sh
@@ -15,7 +15,7 @@ echo "Algorithm,Processes,AvgLatency(us),MinLatency,MaxLatency" >> $out_csv
 for np in {2..48..1}
 do
     # Construct the mpirun command with the current values of np and algorithm
-    command="mpirun -np $np --map-by core --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 5 ${src_path}osu_bcast -x 1000 -i 10000 -m 1:1 -f"
+    command="mpirun -np $np --map-by ppr:1:core:PE=1 --bind-to core --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 5 ${src_path}osu_bcast -x 1000 -i 10000 -m 1:1 -f"
     # Execute the command, extract the numbers and append to the CSV file
     echo "Currentlty benchmarking BinaryTree with n. of processes= $np"
     latency_stats=$(eval $command | tail -n 1 | awk '{gsub(/ +/, ","); sub(/^[^,]+,/, ""); sub(/,[^,]+$/, ""); print}')
