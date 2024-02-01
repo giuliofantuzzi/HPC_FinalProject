5847baf0f9e9675fb6b64fb39e267536c016a5ac
Giulio Fantuzzi <gfantuzzi@login01.hpc.rd.areasciencepark.it>
2024-02-01 13:12:40 +0000

    fixed osu compilation files

--- exercise1/compile_OSU-THIN.sh
+++ exercise1/compile_OSU-THIN.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12
 #SBATCH --time=00:30:00
 #SBATCH --partition=THIN
 #SBATCH --exclusive
 
-cd osu-micro-benchmarks-7.3-THIN
+module load openMPI/4.1.5/gnu
+cd osu-micro-benchmarks-7.3/
 
 ./configure CC=/opt/programs/openMPI/4.1.5/bin/mpicc CXX=/opt/programs/openMPI/4.1.5/bin/mpicxx
 make
