d2bff96ebfb53c4b22aa05ceb4e85cdf056b92d8
Giulio Fantuzzi <gfantuzzi@login01.hpc.rd.areasciencepark.it>
2024-02-18 10:09:53 +0000

    small updates on omp script

--- exercise2/timings/omp_scaling.sh
+++ exercise2/timings/omp_scaling.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-#SBATCH --no-requeue
+##SBATCH --no-requeue
 #SBATCH --job-name="HPC_ex2"
 #SBATCH --get-user-env
 #SBATCH --partition=EPYC
 #SBATCH --nodes=1
 #SBATCH --exclusive
-#SBATCH --time=02:00:00
+#SBATCH --time=00:45:00
 ##SBATCH --nodelist=epyc[007]
 #SBATCH --output=omp_scaling.out
 #--------------------------------------------------------------------------------
-csv="omp_scaling_64M.csv"
+csv="omp_scaling_L1_240M.csv"
 exe="../apps/main.x"
-N=64000000
+N=240000000
 module load architecture/AMD
 module load openMPI/4.1.5/gnu/12.2.1
 #--------------------------------------------------------------------------------
 echo "Processes,Threads,Time" > $csv 
 
-for t in {1..64..2}
+for t in 2 4 8 16 32 64 
 do
     export OMP_NUM_THREADS=$t
     for iter in {1..5..1}
