62d8d273a8186febe1c05814f74a54da40dc00ae
Giulio Fantuzzi <gfantuzzi@login01.hpc.rd.areasciencepark.it>
2024-02-18 13:56:53 +0000

    got data for omp L1 optimization

--- exercise2/timings/omp_scaling.sh
+++ exercise2/timings/omp_scaling.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-##SBATCH --no-requeue
+#SBATCH --no-requeue
 #SBATCH --job-name="HPC_ex2"
-#SBATCH --get-user-env
 #SBATCH --partition=EPYC
 #SBATCH --nodes=1
 #SBATCH --exclusive
-#SBATCH --time=00:45:00
-##SBATCH --nodelist=epyc[007]
+#SBATCH --time=02:00:00
 #SBATCH --output=omp_scaling.out
-#--------------------------------------------------------------------------------
-csv="omp_scaling_L1_240M.csv"
-exe="../apps/main.x"
-N=240000000
+
+csv="omp_scaling_640M.csv"
+exe="/u/dssc/gfantuzzi/HPC_FinalProject/exercise2/apps/main.x"
+N=640000000
 module load architecture/AMD
 module load openMPI/4.1.5/gnu/12.2.1
 #--------------------------------------------------------------------------------
