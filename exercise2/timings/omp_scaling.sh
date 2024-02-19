6a70cfd7c250466e0098185f31f31cbc42c9a299
Giulio Fantuzzi <gfantuzzi@login01.hpc.rd.areasciencepark.it>
2024-02-19 22:26:36 +0000

    fixed typo in omp_scaling.sh

--- exercise2/timings/omp_scaling.sh
+++ exercise2/timings/omp_scaling.sh
@@ -4,21 +4,22 @@
 #SBATCH --partition=EPYC
 #SBATCH --nodes=1
 #SBATCH --exclusive
-#SBATCH --time=02:00:00
+#SBATCH --time=00:15:00
 #SBATCH --output=omp_scaling.out
 
-csv="omp_scaling_640M.csv"
+csv="omp_scaling_L1_640M.csv"
 exe="/u/dssc/gfantuzzi/HPC_FinalProject/exercise2/apps/main.x"
 N=640000000
 module load architecture/AMD
 module load openMPI/4.1.5/gnu/12.2.1
 #--------------------------------------------------------------------------------
-echo "Processes,Threads,Time" > $csv 
+#echo "Processes,Threads,Time" > $csv 
+
 
 for t in 1 2 4 8 16 32 64 
 do
     export OMP_NUM_THREADS=$t
-    for iter in {1..5..1}
+    for iter in {1..10..1}
     do
         $exe $N >> $csv
     done
